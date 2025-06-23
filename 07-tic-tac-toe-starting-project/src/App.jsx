abd5ce46056975b7f89d1aa21f2153c372f62073
NguyenNgocBao <ititiu19084@student.hcmiu.edu.vn>
2025-06-23 13:20:15 +0700

    add activePlayer logic

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleActivePlayer() {
+    setActivePlayer((prevActivePlayer) =>
+      prevActivePlayer === "X" ? "O" : "X"
+    );
+  }
+
   return (
     <div id="game-container">
-      <ol id="players">
-        <Player name="Player 1" symbol="X" />
-        <Player name="Player 2" symbol="O" />
+      <ol id="players" className="highlight-player">
+        <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
+        <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
       </ol>
-      <GameBoard />
+      <GameBoard
+        onActivePlayer={handleActivePlayer}
+        activePlayer={activePlayer}
+      />
     </div>
   );
 }
--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -6,16 +6,18 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onActivePlayer, activePlayer }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelect(rowIndex, colIndex) {
     // This function will handle the logic when a player selects a cell
     setGameBoard((prevState) => {
       const updateState = [...prevState.map((innerArray) => [...innerArray])];
-      updateState[rowIndex][colIndex] = "X";
+      updateState[rowIndex][colIndex] = activePlayer;
       return updateState;
     });
+
+    onActivePlayer();
   }
 
   return (
